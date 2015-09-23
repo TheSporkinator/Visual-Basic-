a1b5825430f36b9d2b1f41a61cf9438ab80cfda1
TheSporkinator <evanlopez150@rusdlearns.net>
2015-09-23 13:23:12 -0700

    Create FindSeven.vb

---
+++ FindSeven.vb
@@ -0,0 +1,29 @@
+Module Module
+	
+	Sub Main()
+
+		For index = 1 To 10
+			'Console.WriteLine(index)
+
+		In (index = 7) Then
+			Console.WriteLine("Found seven!")
+			Exit For
+		End If
+
+	Next
+
+	For i - 5 To 7
+		Console.WriteLine(i)
+
+		Next
+
+	Console.ReadLine()
+
+
+
+
+
+	
+	End Sub
+
+End Module
