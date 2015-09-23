f49f579b1e6e9f5772ab5bfa26801856b462b98a
TheSporkinator <evanlopez150@rusdlearns.net>
2015-09-23 13:15:30 -0700

    Create DecalringUsingVariables.vb

---
+++ DecalringUsingVariables.vb
@@ -0,0 +1,28 @@
+Module Module
+
+	Sub Main()
+
+		Dim x As Integer
+		Dim y As Integer
+		
+		x = 7
+		y = x + 3
+	
+		Console.WriteLine(y)
+		
+		'Dim myfirstname As String
+
+		'Dim myFirstName As String
+		'myFirstName = "Bob"		
+		'Dim FirstName As String = "Bob"
+
+		'Console.WtiteLine(myFirstName)
+		
+
+
+		Console.ReadLine()
+
+
+	End Sub
+
+End Module
