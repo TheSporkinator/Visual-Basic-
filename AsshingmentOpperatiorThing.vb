e28f71f0e824dae9e418e95f1ebf74e602aab042
TheSporkinator <evanlopez150@rusdlearns.net>
2015-09-23 13:20:35 -0700

    Create AsshingmentOpperatiorThing.vb

---
+++ AsshingmentOpperatiorThing.vb
@@ -0,0 +1,60 @@
+Module Module
+	
+	Sub Main()
+		
+		Dim x = 0
+		Dim y = 0
+		Dim a = 0
+		Dim b = 0
+		
+		 ' Assingment operator
+		x = 3
+
+		 ' Addition operator
+		x = 3 + 4
+
+		 ' Subtraction operator
+		x = 4 - 3
+
+		 ' Multiplication operator
+		x = 10 * 5
+		
+		 ' Divison operator
+		x = 10 / 5
+
+		 'Equality operator
+		If x = y Then
+			'Stuf goes here
+		End If
+
+		 'Greater than operator
+		If x > y Then
+			' Stuff goes here
+		
+		End If
+
+		' Greater or equal to operator
+		If x>= y Then
+			' Stuff goes here
+		End if
+
+		' Les or equal to operator
+		If <= y Then
+			' Stuff goes here
+		End If
+
+
+		' Condition Or operator
+
+		If x > y And a > b then
+			' Stuff goes here
+		End If
+
+		' Member access and Method invocation
+		console.WriteLine("Hi!")
+		
+
+
+
+	End Sub
+End Module
