a552ce4d3a6b9c95186453e2c20a9c629c867391
TheSporkinator <evanlopez150@rusdlearns.net>
2015-09-23 13:19:37 -0700

    Create ShortenedDisplayOfMultipleDisplays.vb

---
+++ ShortenedDisplayOfMultipleDisplays.vb
@@ -0,0 +1,45 @@
+Module Module
+
+	Sub Main()
+
+		Console.WriteLine("Please type something and press Enter")
+		Dim userValue As String
+		Dim message = ""
+
+		'userValue - Console.ReadLine()
+		'Console.WriteLine("You typed: " & userValue)
+		'Console.ReadLine()
+
+		'Console.WriteLine("Would you prefer what is behind door number 1, 2 or 3?")
+
+		'Dim userValue As String = Console.ReadLine()
+
+		'If userValue = "1" Then
+		'message = ""You won a new car!""
+
+		''Elself userValue = "2" Then
+		'	message = ""you won a new boat!""
+		
+		'Elself userValue = "3" Then
+		'	message = ""You won a cat!""
+
+		'Else
+		'	message = ""Sorry, we did not understand your response, You lose!"
+
+		'End If
+
+		
+		Console.WriteLine(message)
+		Console.ReadLine()
+
+		Dim message = IIf(userCalue = "1", "car", "cat")
+		Console.WriteLine(You won a {0}!", message)
+		Console.ReadLine()
+
+	
+
+		
+
+	End Sub
+
+End Module
