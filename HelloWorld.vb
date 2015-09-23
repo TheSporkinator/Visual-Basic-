426178b9c5bc7c07fd1bd3fd6785c4d631fe2a79
TheSporkinator <evanlopez150@rusdlearns.net>
2015-09-23 13:14:31 -0700

    Create HelloWorld.vb

---
+++ HelloWorld.vb
@@ -0,0 +1,16 @@
+Module Module 
+	
+	Sub Main()
+
+		Console.WriteLine("Hello World") 'Console is a Method
+		Console.ReadLine()'Console is Class with a method ReadLine()
+		
+		'Console. ReadLine():this line of code will be commented out
+	End Sub
+End Module
+
+
+* notes:
+'<- this is for comments
+'console is a calss with a method
+'Period is a member class
