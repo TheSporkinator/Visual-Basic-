2f66e4a9808388511a589bd2115ed30c901f3326
TheSporkinator <evanlopez150@rusdlearns.net>
2015-09-23 13:17:14 -0700

    Create FirstTryAsString.vb

---
+++ FirstTryAsString.vb
@@ -0,0 +1,18 @@
+Module Module 
+	Sub Main()
+		
+		Dim x As Integer = 7
+		Dim y As String = "5"
+
+		'Dim myFirstTry As String = CStr(x) & y
+		
+		Dim myFirstTry As String = x.ToString() & y
+		Console.WriteLine(myFirstName)
+
+		Dim mySecondTry As Integer = x + CInt(y)
+		Console.WtiteLine(mySecondTry)
+
+		Console.ReadLine()
+	
+	End Sub
+End Module
