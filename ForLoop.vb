81ba01c785924e878ae668b8a44c726074c3195b
TheSporkinator <evanlopez150@rusdlearns.net>
2015-10-02 14:20:31 -0700

    Create ForLoop.vb

---
+++ ForLoop.vb
@@ -0,0 +1,43 @@
+Module Module1
+
+    Sub Main()
+
+        Dim i As Integer = 0
+        Dim j As Integer = 0
+
+        For i = 1 To 10
+
+
+
+            For j = 1 To i ' <-- this makes it go i to # press f10 tosee how it goes back to write down another number
+
+
+                Console.Write(j)
+            Next
+            Console.WriteLine()
+
+
+        Next
+
+        For i = 9 To 1 Step -1
+
+            For j = 1 To i
+
+
+                Console.Write(j)
+            Next
+            Console.WriteLine()
+
+
+        Next
+
+
+
+
+
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
